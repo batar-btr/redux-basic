c54bf82fe0aeadea3da9f952553a41473962802c
batar-btr <btrn86@gmail.com>
2018-04-12 17:38:52 +0300

    dispatching actions

--- redux-basics.js
+++ redux-basics.js
@@ -9,16 +9,45 @@ const initialState = {
 //Reducer
 
 const rootReducer = (state = initialState, action) => {
-    return state
+
+    switch (action.type) {
+        case ('INC_COUNTER'):
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+            break;
+
+        case ('ADD_COUNTER'):
+            return {
+                ...state,
+                counter: state.counter + action.value
+            }
+            break;
+
+        default:
+            return state;
+            break;
+    }
 }
 
 //Store
 
 const store = createStore(rootReducer);
 
-console.log(store.getState());
+//Subscription
+
+store.subscribe(() => {
+    console.log(store.getState())
+})
 
 
 //Dispatching Action
+store.dispatch({ type: 'INC_COUNTER' })
+store.dispatch({
+    type: 'ADD_COUNTER',
+    value: 10
+})
 
-//Subscription
+
+
